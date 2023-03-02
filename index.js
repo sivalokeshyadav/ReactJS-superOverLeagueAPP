1f29af44ab61a84f9b8686bc65ae1f719ea04bee
sivalokeshyadav <222sivalokeshyadav@gmail.com>
2023-03-02 04:35:24 +0000

    superOverLeague

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div className="bg-container">
+    <h1>Super League</h1>
+    <div class="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
